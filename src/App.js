b5933c37c054e8b5367e77ce840cca6274eff840
wesley <wesbourkeavl@gmail.com>
2022-03-28 09:43:25 -0400

    1st repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
+import Header from './components/Header';
+import Game from './components/Game';
 import './App.css';
+import { useRef } from 'react';
+
 
 function App() {
+
+  useEffect( () => {
+
+  
+  },[])
+
+  const soemthing = useRef(null)
+  const startDrawing = () => {
+
+  }
+  const finishDrawing =() => {
+      
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Game 
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        />
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,21 @@
+import React from "react";
+import '../App.css'; 
+
+function Game() {
+
+    const startDrawing = () => {
+
+    }
+
+    const finishDraeing =() => {
+        
+    }
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Game;
